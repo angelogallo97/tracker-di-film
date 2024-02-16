dfd52fbf7b8a252de384622e6eaadd6863cd0ffb
angelogallo97 <125682206+angelogallo97@users.noreply.github.com>
2024-02-16 18:21:17 +0100

    Add files via upload

---
+++ cinebook.js
@@ -0,0 +1,17 @@
+"use strict";
+let cerca = document.getElementById("search_input");
+cerca.addEventListener("keydown", function (event) {
+  console.log(event);
+  if (event.key == "Enter") {
+    console.log("eccomi");
+  }
+});
+
+// Seleziona l'elemento del pulsante
+let myButton = document.getElementById("myButton");
+
+// Aggiungi un listener per l'evento "click" all'elemento del pulsante
+myButton.addEventListener("click", function (event) {
+  // Esegui il tuo codice qui quando il pulsante viene cliccato
+  console.log("Il pulsante è stato cliccato!");
+});
