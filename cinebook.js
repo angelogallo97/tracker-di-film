e269fb435ebe495ac28e7b03da865a14ca8fbec2
angelogallo97 <125682206+angelogallo97@users.noreply.github.com>
2024-02-21 12:57:41 +0100

    Add files via upload

--- cinebook.js
+++ cinebook.js
@@ -1,4 +1,30 @@
 "use strict";
+
+let home = document.getElementById("home");
+home.addEventListener("click", function (event) {
+  console.log("Hai cliccato home");
+});
+
+let chiSiamo = document.getElementById("chi_siamo");
+chiSiamo.addEventListener("click", function (event) {
+  console.log("Hai cliccato Chi siamo");
+});
+
+let film = document.getElementById("film");
+film.addEventListener("clcik", function (event) {
+  console.log("Hai cliccato Film");
+});
+
+let serieTv = document.getElementById("serie_tv");
+serieTv.addEventListener("click", function (event) {
+  console.log("Hai cliccato Serie Tv");
+});
+
+let contatti = document.getElementById("contatti");
+contatti.addEventListener("click", function (event) {
+  console.log("Hai cliccato Contatti");
+});
+
 let cerca = document.getElementById("search_input");
 cerca.addEventListener("keydown", function (event) {
   console.log(event);
@@ -9,7 +35,6 @@ cerca.addEventListener("keydown", function (event) {
 
 // Seleziona l'elemento del pulsante
 let myButton = document.getElementById("myButton");
-
 // Aggiungi un listener per l'evento "click" all'elemento del pulsante
 myButton.addEventListener("click", function (event) {
   // Esegui il tuo codice qui quando il pulsante viene cliccato
