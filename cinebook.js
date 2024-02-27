a98385651aced4d4b3a0c67a1372fae75b985153
angelogallo97 <125682206+angelogallo97@users.noreply.github.com>
2024-02-27 17:55:13 +0100

    Add files via upload

--- cinebook.js
+++ cinebook.js
@@ -11,8 +11,8 @@ chiSiamo.addEventListener("click", function (event) {
 });
 
 let film = document.getElementById("film");
-film.addEventListener("clcik", function (event) {
-  console.log("Hai cliccato Film");
+film.addEventListener("click", function (event) {
+  console.log("Hai cliccato su film");
 });
 
 let serieTv = document.getElementById("serie_tv");
@@ -23,6 +23,14 @@ serieTv.addEventListener("click", function (event) {
 let contatti = document.getElementById("contatti");
 contatti.addEventListener("click", function (event) {
   console.log("Hai cliccato Contatti");
+});
+
+// Seleziona il primo elemento con la classe "lente"
+let lenteIngrandimento = document.getElementsByClassName("lente")[0];
+
+// Aggiungi un event listener al primo elemento della collezione
+lenteIngrandimento.addEventListener("click", function (event) {
+  console.log("Hai cliccato sulla lente");
 });
 
 let cerca = document.getElementById("search_input");
@@ -39,4 +47,16 @@ let myButton = document.getElementById("myButton");
 myButton.addEventListener("click", function (event) {
   // Esegui il tuo codice qui quando il pulsante viene cliccato
   console.log("Il pulsante è stato cliccato!");
+});
+
+// Seleziona l'elemento del menu relativo ai film
+let linkFilm = document.getElementById("film");
+
+// Aggiungi un event listener per il click
+linkFilm.addEventListener("click", function (event) {
+  // Impedisci il comportamento predefinito del link (che è quello di reindirizzare alla pagina specificata nell'attributo href)
+  event.preventDefault();
+
+  // Reindirizza l'utente alla pagina dei film
+  window.location.href = "film.html";
 });
