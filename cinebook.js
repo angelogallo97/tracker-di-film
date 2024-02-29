c57d4b28f4449311159f6368177aa0319a359045
angelogallo97 <125682206+angelogallo97@users.noreply.github.com>
2024-02-29 21:18:34 +0100

    Add files via upload

--- cinebook.js
+++ cinebook.js
@@ -59,6 +59,14 @@ linkFilm.addEventListener("click", function (event) {
   window.location.href = "film.html";
 });
 
+let linkSerieTv = document.getElementById("serie_tv");
+linkSerieTv.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "serieTv.html";
+});
+
+//TRAILER FILM
+
 // Seleziona il pulsante "Visualizza trailer"
 let trailerRocky = document.querySelector(".trailer-button");
 // Aggiungi un event listener per l'evento di click sul pulsante
@@ -140,4 +148,65 @@ trailerHarry.addEventListener("click", function () {
 let trailerUnaPoltrona = document.querySelector(".trailer-button16");
 trailerUnaPoltrona.addEventListener("click", function () {
   window.open("https://www.youtube.com/watch?v=mwcXXpfo6rk", "_blank");
+});
+
+//TRAILER SERIE TV
+let trailerFriends = document.querySelector(".button-trailer");
+trailerFriends.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=813bZYxnwYA", "_blank");
+});
+
+let trailerBreakingBad = document.querySelector(".button-trailer2");
+trailerBreakingBad.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=b8TeqLnXqoA", "_blank");
+});
+
+let trailerPeaky = document.querySelector(".button-trailer3");
+trailerPeaky.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=hGP_ZyrHN68", "_blank");
+});
+
+let trailerWilly = document.querySelector(".button-trailer4");
+trailerWilly.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=js9LsJ7M6L4", "_blank");
+});
+
+let trailerLupin = document.querySelector(".button-trailer5");
+trailerLupin.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=3Zs5ya7zf4w", "_blank");
+});
+
+let trailerTheLast = document.querySelector(".button-trailer6");
+trailerTheLast.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=e1qxCApMN98", "_blank");
+});
+
+let trailerLost = document.querySelector(".button-trailer7");
+trailerLost.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=73I7dJ2F2Nk", "_blank");
+});
+
+let trailerVikings = document.querySelector(".button-trailer8");
+trailerVikings.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=isDJv84NEkA", "_blank");
+});
+
+let trailerNarcos = document.querySelector(".button-trailer9");
+trailerNarcos.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=4zHG8frGRV0", "_blank");
+});
+
+let trailerPrison = document.querySelector(".button-trailer10");
+trailerPrison.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=Ss_DJj9EODs", "_blank");
+});
+
+let trailerGotham = document.querySelector(".button-trailer11");
+trailerGotham.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=rv52V_ssiLw", "_blank");
+});
+
+let trailerTheWalking = document.querySelector(".button-trailer12");
+trailerTheWalking.addEventListener("click", function () {
+  window.open("https://www.youtube.com/watch?v=Lflm3Z7uR1M", "_blank");
 });
