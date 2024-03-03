705b52d979babd2b6812bceca561b8f8b42e559e
angelogallo97 <125682206+angelogallo97@users.noreply.github.com>
2024-03-03 22:42:55 +0100

    Add files via upload

---
+++ contatti.js
@@ -0,0 +1,6 @@
+//Collegamento alla pagina "Home"
+let linkHome = document.getElementById("home");
+linkHome.addEventListener("click", function (event) {
+  event.preventDefault();
+  window.location.href = "cinebook.html";
+});
