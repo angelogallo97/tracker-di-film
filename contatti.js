a271b9d4e2811dd55f1354ea8372827d36e07a4f
angelogallo97 <125682206+angelogallo97@users.noreply.github.com>
2024-03-04 16:47:02 +0100

    Add files via upload

--- contatti.js
+++ contatti.js
@@ -4,3 +4,16 @@ linkHome.addEventListener("click", function (event) {
   event.preventDefault();
   window.location.href = "cinebook.html";
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleziona il modulo di candidatura
+  let moduloCandidatura = document.getElementById("modulo-candidatura");
+  // Aggiungi un gestore evento per il submit del modulo
+  moduloCandidatura.addEventListener("submit", function (event) {
+    // Impedisci il comportamento predefinito del modulo
+    event.preventDefault();
+    // Visualizza un messaggio simile a un alert
+    alert("Candidatura inviata correttamente");
+    console.log("Hai inviato la candidatura");
+  });
+});
